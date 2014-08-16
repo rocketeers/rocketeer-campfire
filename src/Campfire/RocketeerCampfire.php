463509eeb1bb904213eebc6b6db19eff6af496c0
Maxime Fabre <ehtnam6@gmail.com>
2014-08-16 20:38:31 +0200

    Update for Rocketeer 2

--- src/Campfire/RocketeerCampfire.php
+++ src/Campfire/RocketeerCampfire.php
@@ -3,55 +3,59 @@ namespace Rocketeer\Plugins\Campfire;
 
 use Illuminate\Container\Container;
 use rcrowe\Campfire;
-use Rocketeer\Plugins\Notifier;
+use Rocketeer\Plugins\AbstractNotifier;
 
-class RocketeerCampfire extends Notifier
+class RocketeerCampfire extends AbstractNotifier
 {
-  /**
-   * Setup the plugin
-   */
-  public function __construct(Container $app)
-  {
-    parent::__construct($app);
+	/**
+	 * Setup the plugin
+	 *
+	 * @param Container $app
+	 */
+	public function __construct(Container $app)
+	{
+		parent::__construct($app);
 
-    $this->configurationFolder = __DIR__.'/../config';
-  }
+		$this->configurationFolder = __DIR__.'/../config';
+	}
 
-  /**
-   * Bind additional classes to the Container
-   *
-   * @param Container $app
-   *
-   * @return void
-   */
-  public function register(Container $app)
-  {
-    $app->bind('campfire', function ($app) {
-      return new Campfire($app['config']->get('rocketeer-campfire::config'));
-    });
+	/**
+	 * Bind additional classes to the Container
+	 *
+	 * @param Container $app
+	 *
+	 * @return void
+	 */
+	public function register(Container $app)
+	{
+		$app->bind('campfire', function ($app) {
+			return new Campfire($app['config']->get('rocketeer-campfire::config'));
+		});
 
-    return $app;
-  }
+		return $app;
+	}
 
-  /**
-   * Get the default message format
-   *
-   * @return string
-   */
-  protected function getMessageFormat()
-  {
-    return $this->app['config']->get('rocketeer-campfire::message');
-  }
+	/**
+	 * Get the default message format
+	 *
+	 * @param string $message
+	 *
+	 * @return string
+	 */
+	protected function getMessageFormat($message)
+	{
+		return $this->app['config']->get('rocketeer-campfire::'.$message);
+	}
 
-  /**
-   * Send a given message
-   *
-   * @param string $message
-   *
-   * @return void
-   */
-  public function send($message)
-  {
-    $this->app['campfire']->send($message);
-  }
+	/**
+	 * Send a given message
+	 *
+	 * @param string $message
+	 *
+	 * @return void
+	 */
+	public function send($message)
+	{
+		$this->app['campfire']->send($message);
+	}
 }
